5175eb9b43a232ce32a75076d262fc321cf60f37
Shivratan <164228363+shivbera18@users.noreply.github.com>
2025-02-05 13:10:11 +0530

    Create VanillaRNN_pytorch.py

---
+++ RNN/VanillaRNN_pytorch.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn 
+import matplotlib.pyplot as plt 
+
+class RNN(nn.Module):
+    # implement RNN from scratch rather than using nn.RNN
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+        
+    def forward(self, input_tensor, hidden_tensor):
+        combined = torch.cat((input_tensor, hidden_tensor), 1)
+        
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+    
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+      
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+'''
+this is just the basic block. we can also increase complexity and add more training loops ad vary input
+'''
